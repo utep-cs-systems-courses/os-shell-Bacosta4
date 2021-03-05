ae1a2f0a9174773ea0062d6b595e2dfbaf804faa
bacosta4 <bacosta4@miners.utep.edu>
2021-03-05 00:34:05 -0700

    small error

--- shell/readline.py
+++ shell/readline.py
@@ -1,24 +1,25 @@
+#! /usr/bin/env python3
+
 from os import read
 
 next = 0
 limit = 0
 
-def gChar():
+def getChar():
     global next
     global limit
 
-    if (next == limit):
+    if next == limit:
         next = 0
         limit = read(0,1000)
 
-        if(limit == 0):
+        if limit == 0:
             return "EOF"
 
-        if (next < len(limit) -1):
+        if next < len(limit) -1:
             c = chr(limit[next])
             next += 1
             return c
-
         else:
             return "EOF"
 
